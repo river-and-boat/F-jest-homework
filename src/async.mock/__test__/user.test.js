5d47b04607ab48e63a42dffa28d025ce82fa7a6d
mengqiu PENG <mengqiu.peng@thoughtworks.com>
2020-09-24 12:15:10 +0800

    [mengqiu]  add feedback

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,18 +1,17 @@
-// eslint-disable-next-line no-unused-vars
-import { register, validate } from "../user";
+import { register } from "../user";
 import { verifyUsername } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
+  test("should post user when validated", async () => {
     // TODO 19: add test here
     const username = "mocked username";
     const password = "mock password";
     const result = register(username, password);
-    // eslint-disable-next-line jest/valid-expect
-    expect(result).resolves.toContain("success");
+    // TODO feedback: 因为是异步，所以需要用到async/await
+    await expect(result).resolves.toContain("success");
   });
 
   test("should reject with Error when username is invalid", async () => {
--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -1,12 +1,14 @@
 import getUsers from "../users";
-// eslint-disable-next-line jest/no-mocks-import
-import mockAxios from "../../async.mock/__mocks__/axios";
+// TODO feedback: axios是我们装的node module, 不是我们自己写的module, 它的mock文件要放在src/下面
 
+jest.mock("axios");
 describe("users", () => {
   test("should get users data with mock axios get", async () => {
     // TODO 13: add async test with manual mock
     const result = getUsers();
-    expect(mockAxios.get).toBeCalledTimes(1);
+    // TODO feedback: 没必要测被调用的次数，因为你都测调用的返回值
+
+    // expect(mockAxios.get).toBeCalledTimes(1);
     await expect(result).resolves.toEqual({});
   });
 });
